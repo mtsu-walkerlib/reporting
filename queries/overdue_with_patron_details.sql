9271f0c78b3017d6ff5a6b5fe5b8dec6bcabaf17
Clay Oldham <97476273+rollplayr@users.noreply.github.com>
2025-12-08 09:44:05 -0600

    Fix SQL query to select due_date correctly

--- queries/overdue_with_patron_details.sql
+++ queries/overdue_with_patron_details.sql
@@ -21,7 +21,7 @@ returns table
 )
 as
 $$
-SELECT cast(li.loan_due_date::date::text) AS due_date,
+SELECT  li.loan_due_date::date::text AS due_date,
 	   cast(li.loan_date as DATE) as check_out_date,
         ihi.barcode AS item_barcode,
 		ug.username as patron_username,
