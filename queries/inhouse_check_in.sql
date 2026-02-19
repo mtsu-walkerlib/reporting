e1fabc0809223bfedc33c9bf1d76b899b8b8ccc1
Clay Oldham <97476273+rollplayr@users.noreply.github.com>
2026-02-19 14:04:17 -0600

    Update date range for check-in query

--- queries/inhouse_check_in.sql
+++ queries/inhouse_check_in.sql
@@ -30,8 +30,8 @@ LEFT JOIN folio_inventory.holdings_record__t AS h  ON h.id  = i.holdings_record_
 LEFT JOIN folio_inventory.instance__t       AS inst ON inst.id = h.instance_id
 LEFT JOIN folio_users.users__t              AS u   ON u.id  = cit.performed_by_user_id
 WHERE cit.item_status_prior_to_check_in = 'Available'
-  AND cit.occurred_date_time >= TIMESTAMP '2025-01-01 00:00:00'
-  AND cit.occurred_date_time <  TIMESTAMP '2026-01-01 00:00:00'
+  AND cit.occurred_date_time >= TIMESTAMP '2026-01-01 00:00:00'
+  AND cit.occurred_date_time <  TIMESTAMP '2027-01-01 00:00:00'
 ORDER BY cit.occurred_date_time;
 $$
 LANGUAGE SQL STABLE;
