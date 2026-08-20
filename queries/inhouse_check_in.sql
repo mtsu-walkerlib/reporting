8d2710411651a4167ef5b9f5b5947e2a3de73ba1
Clay Oldham <97476273+rollplayr@users.noreply.github.com>
2026-08-20 13:14:49 -0500

    Modify inhouse_check_in function to accept date parameters

--- queries/inhouse_check_in.sql
+++ queries/inhouse_check_in.sql
@@ -1,7 +1,7 @@
 --metadb:function inhouse_check_in
 DROP FUNCTION IF EXISTS inhouse_check_in;
 
-CREATE FUNCTION inhouse_check_in()
+CREATE FUNCTION inhouse_check_in(start_date DATE, end_date DATE)
 
 returns table
 (
@@ -30,8 +30,8 @@ LEFT JOIN folio_inventory.holdings_record__t AS h  ON h.id  = i.holdings_record_
 LEFT JOIN folio_inventory.instance__t       AS inst ON inst.id = h.instance_id
 LEFT JOIN folio_users.users__t              AS u   ON u.id  = cit.performed_by_user_id
 WHERE cit.item_status_prior_to_check_in = 'Available'
-  AND cit.occurred_date_time >= TIMESTAMP '2026-01-01 00:00:00'
-  AND cit.occurred_date_time <  TIMESTAMP '2027-01-01 00:00:00'
+  AND cit.occurred_date_time >= start_date::timestamp
+  AND cit.occurred_date_time <  (end_date + INTERVAL '1day')::timestamp
 ORDER BY cit.occurred_date_time;
 $$
 LANGUAGE SQL STABLE;
